c28d4b861bf308f34fb43d51e6211ef4db5768e7
Arfan12630 <arfanrahman464@gmail.com>
2025-05-02 22:19:59 -0400

    Added functionality to create tables

--- client/savr-client/src/components/RestuarantLayout/Navbar/NavBar.tsx
+++ client/savr-client/src/components/RestuarantLayout/Navbar/NavBar.tsx
@@ -7,7 +7,7 @@ import Divider from '@mui/joy/Divider';
 import ListItem from '@mui/joy/ListItem';
 import ListItemButton from '@mui/joy/ListItemButton';
 
-const NavBar = () => {
+const NavBar = ({addChair}: {addChair: () => void}) => {
   const [open, setOpen] = React.useState(false);
 
   const toggleDrawer =
@@ -23,32 +23,62 @@ const NavBar = () => {
       setOpen(inOpen);
     };
 
+   
   return (
     <Box sx={{ display: 'flex' }}>
       <Button variant="outlined" color="neutral" onClick={toggleDrawer(true)}>
-        Customize
+        Edit Layout
       </Button>
       <Drawer open={open} onClose={toggleDrawer(false)}>
         <Box
           role="presentation"
-          onClick={toggleDrawer(false)}
-          onKeyDown={toggleDrawer(false)}
+       
+          sx={{
+            minWidth: 260,
+            bgcolor: 'background.body',
+            height: '100%',
+            boxShadow: 3,
+            borderRadius: 2,
+            p: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
         >
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
-              <ListItem key={text}>
-                <ListItemButton>{text}</ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text) => (
-              <ListItem key={text}>
-                <ListItemButton>{text}</ListItemButton>
-              </ListItem>
-            ))}
-          </List>
+          <Box sx={{ mb: 3, fontWeight: 'bold', fontSize: 24, letterSpacing: 1 }}>
+            <Button variant="outlined" color="neutral" onClick={toggleDrawer(false)}>
+            Exit
+            </Button>
+           
+          </Box>
+
+          <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: 1 }}>
+         
+              <Button
+                onClick={() => addChair()}
+                variant="soft"
+                color="primary"
+                sx={{
+                  borderRadius: 3,
+                  fontWeight: 500,
+                  fontSize: 16,
+                  boxShadow: 1,
+                  transition: 'all 0.2s',
+                  '&:hover': {
+                    bgcolor: 'primary.solidBg',
+                    color: 'white',
+                    boxShadow: 3,
+                    transform: 'translateY(-2px) scale(1.03)',
+                  },
+                }}
+                fullWidth
+              >
+                Add Chair
+              </Button>
+          </Box>
+           
+
+          
         </Box>
       </Drawer>
     </Box>
