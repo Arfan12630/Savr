7c99359828f53ed084df799066ab0ff032231e28
Arfan12630 <arfanrahman464@gmail.com>
2025-05-07 13:14:13 -0400

    User view for restuarant layout available

--- client/savr-client/src/App.tsx
+++ client/savr-client/src/App.tsx
@@ -1,19 +1,35 @@
 import React from 'react'
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { DndContext } from '@dnd-kit/core';
 import NavBar from './components/RestuarantLayout/Navbar/NavBar';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
 // import logo from './logo.svg';
-import DroppableArea from './components/DroppableRestuarant';
+import DroppableArea from './components/RestuarantLayout/DroppableRestuarant';
 import './App.css';
 import RestuarantLayout from './components/RestuarantLayout/ChairLayout';
+import TempHomeFile from './components/HomeLayout/TempHomeFile';
+import ViewRestaurantLayout from './components/RestuarantViewLayout/ViewRestaurantLayout';
+
 function App() {
   return (
-    <div>
-      <DndContext>
-        <DroppableArea />
-      </DndContext>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TempHomeFile />} />
+        <Route
+          path="/edit"
+          element={
+            <DndContext>
+              <DroppableArea />
+            </DndContext>
+          }
+        />
+        <Route
+          path="/view"
+          element={
+              <ViewRestaurantLayout />
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/savr-client/src/components/RestuarantViewLayout/ViewRestaurantLayout.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ChairLayout from '../RestuarantLayout/ChairLayout';
+import TableLayout from '../RestuarantLayout/TableLayout';
+const ViewRestaurantLayout: React.FC = () => {
+  const [layout, setLayout] = useState<{ chairs: any[]; tables: any[] } | null>(null);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/get-layout')
+      .then(response => setLayout(response.data))
+      .catch(error => console.error('Error fetching layout:', error));
+  }, []);
+
+  if (!layout) return <div>Loading...</div>;
+
+  return (
+    <div style={{ width: '100vw', height: '100vh', position: 'fixed', top: 0, left: 0 }}>
+      {layout.chairs.map(chair => (
+        <ChairLayout viewOnly={true} key={chair.id} id={chair.id} position={{ x: chair.x, y: chair.y }} onDelete={() => {}} />
+      ))}
+      {layout.tables.map(table => (
+        <TableLayout viewOnly={true} key={table.id} id={table.id} position={{ x: table.x, y: table.y }} onDelete={() => {}} />
+      ))}
+    </div>
+  );
+};
+
+export default ViewRestaurantLayout;
