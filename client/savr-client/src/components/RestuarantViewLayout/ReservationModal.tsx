52a80bf92ae1016746aa3012dbab7fc647507572
Arfan12630 <arfanrahman464@gmail.com>
2025-05-21 13:25:43 -0400

    Added reservation feature

---
+++ client/savr-client/src/components/RestuarantViewLayout/ReservationModal.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Button from '@mui/joy/Button';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import DialogTitle from '@mui/joy/DialogTitle';
+import DialogContent from '@mui/joy/DialogContent';
+import Stack from '@mui/joy/Stack';
+import Add from '@mui/icons-material/Add';
+
+const ReservationModal: React.FC<{
+  open: boolean;
+  onClose: () => void;
+  onReserve: () => void;
+  onCancel?: () => void;
+  reserved?: boolean;
+}> = ({ open, onClose, onReserve, onCancel, reserved }) => {
+  return (
+    <React.Fragment>
+      <Modal open={open} onClose={onClose}>
+        <ModalDialog>
+          <DialogTitle>
+            {reserved ? 'Cancel Reservation' : 'Reserve Table'}
+          </DialogTitle>
+          <DialogContent>
+            {reserved
+              ? 'Are you sure you want to cancel this reservation?'
+              : 'Fill in the reservation information.'}
+          </DialogContent>
+          <form
+            onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+              event.preventDefault();
+              if (reserved && onCancel) {
+                onCancel();
+              } else {
+                onReserve();
+              }
+            }}
+          >
+            <Stack spacing={2}>
+              {!reserved && (
+                <>
+                  <FormControl>
+                    <FormLabel>Name</FormLabel>
+                    <Input autoFocus required />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>Description</FormLabel>
+                    <Input required />
+                  </FormControl>
+                </>
+              )}
+              <Button type="submit">
+                {reserved ? 'Cancel Reservation' : 'Reserve'}
+              </Button>
+            </Stack>
+          </form>
+        </ModalDialog>
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+export default ReservationModal;
