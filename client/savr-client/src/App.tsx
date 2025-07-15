096968bbe4bfd874546242d131a08183bd2c3f0d
Arfan12630 <arfanrahman464@gmail.com>
2025-07-15 17:11:13 -0400

    Added checkout UI

--- client/savr-client/src/App.tsx
+++ client/savr-client/src/App.tsx
@@ -15,6 +15,7 @@ import MenuCardDisplay from './components/MenuDisplay/MenuCardDisplay';
 import PolygonTest from './components/RestuarantLayout/PolygonTest';
 import SignIn from './components/LoginFunctionality/SignIn';
 import SignUp from './components/LoginFunctionality/SignUp';
+import Checkout from './components/Checkout/Checkout';
 function App() {
   return (
     <Router>
@@ -26,6 +27,7 @@ function App() {
         <Route path="owner/restaurant-entry/menu-image-upload" element={<RestuarantMenuImageUpload />} />
         {/* <Route path="/menu-display" element={<MenuDisplay />} /> */}
         <Route path="/menu-card-display" element={<MenuCardDisplay />} />
+        <Route path="/checkout" element={<Checkout />} />
 <Route path="/polygon-test" element={<PolygonTest label="Kitchen" top={100} left={100} width={300} height={200}  />} />
         <Route
           path="/edit"
